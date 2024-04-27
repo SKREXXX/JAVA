f0094e33b5e0e04c30e0cacf1ed9ef4680b3405d
Natan Lima <168303588+SKREXXX@users.noreply.github.com>
2024-04-27 16:43:37 -0300

    Add files via upload

---
+++ SET.java
@@ -0,0 +1,30 @@
+
+package prova.a3;
+public class ProvaA3{
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+        
+        p1.setNome("Natan");
+        p2.setNome("Nathalia");
+        p3.setNome("Nathan");
+        p4.setNome("Maew");
+        
+        p1.setSexo ("M");
+        p4.setSexo("F");
+        p2.setIdade(22);
+        p3.setIdade(15);
+        
+        p3.setSalario(2545.54f);
+        p2.setCurso("ADS DEV");
+        
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+          }
+    
+}
